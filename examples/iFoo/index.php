086528a397cebcf6e95300f863f476ae6e68e17f
Antoine07 <antoine.lucsko@wanadoo.fr>
2013-11-19 20:40:07 +0100

    dir example

--- examples/iFoo/index.php
+++ examples/iFoo/index.php
@@ -7,7 +7,8 @@ var_dump((new Foo)->setFoo(new Baz1));
 var_dump((new Foo)->setFoo(new Baz2));
 var_dump((new Foo)->setFoo(new Baz3));
 
-// la classe Baz4 ne respecte pas le contrat
+class Baz4{}
+// la classe Baz4 ne respecte pas le contrat BADABOUM...
 var_dump((new Foo)->setFoo(new Baz4));
 
 ?>
